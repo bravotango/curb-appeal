eaa054c01ceadda55f00c25f4c1596d375fd4566
seattleriot <seattleriot@hotmail.com>
2023-07-13 14:48:50 -0700

    Fixed SCSS scroller issues and header padding

--- src/components/Scroller.tsx
+++ src/components/Scroller.tsx
@@ -34,12 +34,14 @@ const Scroller: React.FC<ScrollerProps> = ({ articles }) => {
     <div className='scroller-container'>
       <div className='scroller' ref={scrollRef}>
         <FaChevronLeft className='scroll-left' onClick={handleScrollLeft} />
+
         {articles.map((article, index) => (
           <div key={index} className='article'>
             <img src={article.imageUrl} alt={article.title} />
             <h3>{article.title}</h3>
           </div>
         ))}
+
         <FaChevronRight className='scroll-right' onClick={handleScrollRight} />
       </div>
     </div>
