3b34883633087d986efca5445b86c19b862abf55
seattleriot <seattleriot@hotmail.com>
2023-08-30 00:12:15 -0700

    updated h2font

--- client/src/components/pages/Contact.tsx
+++ client/src/components/pages/Contact.tsx
@@ -2,13 +2,13 @@ const Contact = () => {
   return (
     <div className='contact-info'>
       <h1>Contact</h1>
-
+      <h2>Email:</h2>
       <p>
-        <strong>Email:</strong>{' '}
         <a href='mailto:info.curbAppeal@gmail.com'>info.curbAppeal@gmail.com</a>
-      </p>
+      </p>{' '}
+      <h2>Call:</h2>
       <p>
-        <strong>Call:</strong> <a href='tel:+4257732818'>(425) 773-2818</a>
+        <a href='tel:+4257732818'>(425) 773-2818</a>
       </p>
     </div>
   );
